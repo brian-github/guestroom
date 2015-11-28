1d8ebed6ac8e0b9f84718a0101226bad1c8e967b
Brian Judge <judgebh@gmail.com>
2015-11-28 15:59:17 -0600

    fixed bug with selecting date from calendar

--- client/views/calendar.js
+++ client/views/calendar.js
@@ -42,6 +42,9 @@ Template.calendar.helpers({
       if(day < 10) {
         day = "0"+day;
       }
+      if(month < 10) {
+        month = "0"+month;
+      }
       $('#date').val(year + "-" + month + "-" + day);
     }
   }
